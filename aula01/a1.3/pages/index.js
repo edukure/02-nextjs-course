3e831aa4d6b428542c8dbdeeaac20348dc2a23a0
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-01-25 00:54:13 -0300

    fim A104

--- aula01/a1.3/pages/index.js
+++ aula01/a1.3/pages/index.js
@@ -1,5 +1,21 @@
+import Link from 'next/link'
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <div>
+      Welcome to Next.js!
+
+      <img src="/images/avatar.png" />
+
+      <ul>
+        <li>
+          <Link href="/sobre">
+            <a>Ir para a /sobre</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default HomePage
