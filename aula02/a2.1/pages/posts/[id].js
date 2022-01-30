0d52fa40f353c88a103094c52a637920cef84ba0
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-01-30 15:03:35 -0300

    final aula2.2

--- aula02/a2.1/pages/posts/[id].js
+++ aula02/a2.1/pages/posts/[id].js
@@ -1,16 +1,62 @@
 import NextLink from 'next/link';
 import { Box, Text } from '@skynexui/components';
 import { useRouter } from 'next/router';
+import dados from '../../dados.json';
 
-export default function PostByIdScreen() {
+// dica dos paths estáticos
+export async function getStaticPaths() {
+  // const paths = [
+  //   { params: { id: '1' } },
+  //   { params: { id: '2' } },
+  //   { params: { id: '3' } }
+  // ]
+  const paths = dados.posts.map((postAtual) => {
+    return { params: { id: `${postAtual.id}` } };
+  })
+  console.log('dados:', dados);
+  console.log('paths:', paths);
+
+  return {
+    paths: paths,
+    fallback: false // false or 'blocking'
+  };
+}
+
+export async function getStaticProps(context) {
+  console.log('Contexto', context.params.id);
+  const id = context.params.id;
+
+  const post = dados.posts.find((currentPost) => {
+    if(currentPost.id === id) {
+      return true;
+    }
+    return false;
+  })
+
+  console.log(post);
+
+  return {
+    props: {
+      id: post.id,
+      title: post.title,
+      date: post.date,
+      content: post.content,
+    }, 
+  }
+}
+
+export default function PostByIdScreen(props) {
+  // console.log(props);
   const router = useRouter();
+  // console.log(router);
   const post = {
-    title: `Post: [${router.query.id}]`,
-    date: `11/11/1111`,
-    content: `
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lorem urna, laoreet sed magna ac, scelerisque rhoncus nulla. Morbi rhoncus venenatis elementum. Phasellus nec purus ut nisi tempus sagittis sed eget lacus. Nullam in nibh porttitor, dictum metus placerat, molestie augue. Phasellus a mi fermentum erat tempor sollicitudin. Donec eget elementum leo. Morbi sit amet enim varius, ultrices leo commodo, faucibus purus.
-      Vestibulum ut sem est. Phasellus sodales rhoncus convallis. Etiam sit amet libero non tellus vestibulum bibendum. Integer ac eros vel justo imperdiet dictum. Etiam dapibus dolor augue, a suscipit metus sollicitudin in. Integer suscipit ac justo eget molestie. Vivamus id auctor lectus. Ut malesuada est quis urna mollis semper. Nulla cursus, metus eget elementum venenatis, tortor massa tincidunt justo, ac faucibus tortor felis id lacus. Sed quis dolor sem. Sed cursus justo eu accumsan rhoncus. Nam eget lectus libero. Nulla nulla leo, iaculis sagittis massa non, malesuada porta velit. Fusce vel accumsan nisi.
-    `,
+    title: props.title,
+    date: props.date,
+    content: props.content,
+  };
+
+  if(router.isFallback) {
+    return 'Essa página não existe!';
   }
 
   return (
